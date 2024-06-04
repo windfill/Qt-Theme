bbee560b1ae676762a70cc110f09d4ff821bde89
windfill <57623586+windfill@users.noreply.github.com>
2024-06-04 11:34:01 +0800

    Update OpenCreateStylesDir.ps1

--- OpenCreateStylesDir.ps1
+++ OpenCreateStylesDir.ps1
@@ -7,5 +7,5 @@ if (-not (Test-Path $folderPath)) {
 Invoke-Item -Path $folderPath
 
 # 暂停并等待用户按下任意键
-Write-Host "Press any key to exit..."
-$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp")
+# Write-Host "Press any key to exit..."
+# $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp")
